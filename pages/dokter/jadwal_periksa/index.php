7bb4d2812474e5bea830dc4f87c1ef064620b1d9
rayhanashlikh <rayhanind24@gmail.com>
2023-12-15 23:22:16 +0700

    update halaman jadwal periksa

--- pages/dokter/jadwal_periksa/index.php
+++ pages/dokter/jadwal_periksa/index.php
@@ -17,3 +17,90 @@ if ($akses != 'dokter') {
   die();
 }
 ?>
+
+<?php
+$title = 'Poliklinik | Jadwal Periksa';
+// Breadcrumb section
+ob_start();?>
+<ol class="breadcrumb float-sm-right">
+  <li class="breadcrumb-item"><a href="<?= $base_dokter; ?>">Home</a></li>
+  <li class="breadcrumb-item active">Jadwal Periksa</li>
+</ol>
+<?php
+$breadcrumb = ob_get_clean();
+ob_flush();
+
+// Title Section
+ob_start();?>
+Jadwal Periksa
+<?php
+$main_title = ob_get_clean();
+ob_flush();
+
+// Content section
+ob_start();
+?>
+<div class="card">
+  <div class="card-header">
+    <div class="row">
+      <div class="col-6">
+        <h3 class="card-title">Daftar Jadwal Periksa</h3>
+      </div>
+      <div class="col-6">
+        <a href="#" class="btn btn-primary btn-sm float-right"><i class="fa fa-plus"></i> Tambah Jadwal Periksa</a>
+      </div>
+    </div>
+  </div>
+  <div class="card-body">
+    <table id="tabel-jadwal-periksa" class="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Nama Dokter</th>
+          <th>Hari</th>
+          <th>Jam Mulai</th>
+          <th>Jam Selesai</th>
+          <th>Aksi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $no = 1;
+        $data = $pdo->prepare("SELECT 
+                                d.nama as nama_dokter, 
+                                p.hari as hari,
+                                p.jam_mulai as jam_mulai,
+                                p.jam_selesai as jam_selesai
+                                FROM jadwal_periksa p INNER JOIN dokter d ON p.id_dokter = d.id
+                                WHERE d.nama = '$nama'");
+        $data->execute();
+        if ($data->rowCount() == 0) {
+          echo "<tr><td colspan='7' align='center'>Tidak ada data</td></tr>";
+        } else {
+          while($d = $data->fetch()) {
+        ?>
+        <tr>
+          <td><?= $no++; ?></td>
+          <td><?= $d['nama_dokter'] ?></td>
+          <td><?= $d['hari'] ?></td>
+          <td><?= $d['jam_mulai'] ?></td>
+          <td><?= $d['jam_selesai'] ?></td>
+          <td>
+            <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> Edit</a>
+            <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i> Hapus</a>
+          </td>
+        </tr>
+        <?php
+          }
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+<?php
+$content = ob_get_clean();
+ob_flush();
+?>
+
+<?php include_once("../../../layouts/index.php"); ?>
