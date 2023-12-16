531374844d4cc53c74817326558bf78de4f80d28
muhammadroynaldi <111202113471@mhs.dinus.ac.id>
2023-12-16 19:53:53 +0700

    add index periksa pasien

--- pages/dokter/memeriksa_pasien/index.php
+++ pages/dokter/memeriksa_pasien/index.php
@@ -16,4 +16,82 @@ if ($akses != 'dokter') {
   echo "<meta http-equiv='refresh' content='0; url=..'>";
   die();
 }
+
+$pasien = query("SELECT  daftar_poli.no_antrian AS no_antrian, pasien.nama AS nama_pasien, daftar_poli.keluhan AS keluhan 
+                 FROM pasien 
+                 INNER JOIN daftar_poli ON pasien.id = daftar_poli.id_pasien");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= getenv('APP_NAME') ?> | Profil</title>
+
+  <?php include "../../../layouts/plugin_header.php" ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="http://<?= $_SERVER['HTTP_HOST'] ?>/bk-poliklinik/dist/img/Logo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+
+    <?php include "../../../layouts/header.php" ?>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <!-- <h1 class="m-0">Profil <?= ucwords($_SESSION['akses']) ?></h1> -->
+              <h1 class="m-0">Daftar Periksa</h1>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="card">
+          <div class="card-body p-0">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th style="width: 8%">No Urut</th>
+                  <th style="width: 40%">Nama Pasien</th>
+                  <th style="width: 40%">Keluhan</th>
+                  <th style="width: 15%">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach ($pasien as $pasiens) : ?>
+                  <tr>
+                    <td class="text-center"><?= $pasiens["no_antrian"] ?></td>
+                    <td><?= $pasiens["nama_pasien"] ?></td>
+                    <td><?= $pasiens["keluhan"] ?></td>
+
+                    <td>
+                      <button type="button" class="btn btn-warning">Edit</button>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+      <!-- /.content -->
+    </div>
+    <?php include "../../../layouts/footer.php"; ?>
+  </div>
+  <?php include "../../../layouts/pluginsexport.php"; ?>
+</body>
+
+</html>
