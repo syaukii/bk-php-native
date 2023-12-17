e8c022596bf82d35c5e71a4b652b3baab1382829
Hypes-Astro <muhamadalifanwar@gmail.com>
2023-12-17 22:55:12 +0700

    kelola pasien : adding auto no_rm for admin add data, ubah can get data from past, solved bug

--- pages/admin/pasien/index.php
+++ pages/admin/pasien/index.php
@@ -36,78 +36,95 @@ Tambah / Edit Pasien
 $main_title = ob_get_clean();
 ob_flush();
 
+
 // Content section
 ob_start();
-?>
-<form class="form col" method="POST" action="" name="myForm" onsubmit="return(validate());">
-        <?php
-        $nama = '';
-        $alamat = '';
-        $no_hp = '';
-        $no_ktp = '';
-        $no_rm = '';
-        if (isset($_GET['id'])) {
-            try {
-                $stmt = $pdo->prepare("SELECT * FROM pasien WHERE id = :id");
-                $stmt->bindParam(':id', $_GET['id']);
-                $stmt->execute();
 
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $nama = $row['nama'];
-                    $alamat = $row['alamat'];
-                    $no_hp = $row['no_hp'];
-                    $no_ktp = $row['no_ktp'];
-                    $no_rm = $row['no_rm'];
-                }
-            } catch (PDOException $e) {
-                echo "Error: " . $e->getMessage();
+?>
+<form class="form col" method="POST" action="" required name="myForm" onsubmit="return(validate());">
+<?php
+    $nama = '';
+    $alamat = '';
+    $no_hp = '';
+    $no_ktp = '';
+    $no_rm = '';
+    if (isset($_GET['id'])) {
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM pasien WHERE id = :id");
+            $stmt->bindParam(':id', $_GET['id']);
+            $stmt->execute();
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $nama = $row['nama'];
+                $alamat = $row['alamat'];
+                $no_hp = $row['no_hp'];
+                $no_ktp = $row['no_ktp'];
+                $no_rm = $row['no_rm'];
             }
-        ?>
-        <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
-        <?php
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
         }
-        ?>
+    ?>
+        <input type="hidden" required name="id" value="<?php echo $_GET['id'] ?>">
+
+    <?php
+    }
+    
+    // Jika sedang dalam mode ubah, isi Nomor RM sesuai data yang diubah
+    if (isset($_GET['id'])) {
+        $no_rm = $no_rm;
+    } else {
+        // Jika menambahkan data baru, hitung Nomor RM sesuai format
+        $tahun_bulan = "20" . date("ym");
+        $query_last_id = "SELECT MAX(id) as max_id FROM pasien";
+        $result_last_id = $pdo->query($query_last_id);
+        $row_last_id = $result_last_id->fetch(PDO::FETCH_ASSOC);
+        $last_inserted_id = $row_last_id['max_id'] ? $row_last_id['max_id'] : 0;
+
+        $no_rm = $tahun_bulan . "-" . ($last_inserted_id + 1);
+    }
+    ?>
         <div class="row mt-3">
             <label for="nama" class="form-label fw-bold">
                 Nama Pasien
             </label>
-            <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama Dokter" value="<?php echo $nama ?>">
+            <input type="text" class="form-control" required name="nama" id="nama" placeholder="Nama Pasien" value="<?php echo $nama ?>">
         </div>
         <div class="row mt-3">
             <label for="alamat" class="form-label fw-bold">
                 Alamat
             </label>
-            <input type="text" class="form-control" name="alamat" id="alamat" placeholder="alamat" value="<?php echo $alamat ?>">
+            <input type="text" class="form-control" required name="alamat" id="alamat" placeholder="alamat" value="<?php echo $alamat ?>">
         </div>
         <div class="row mt-3">
             <label for="no_ktp" class="form-label fw-bold">
                 Nomor KTP
             </label>
-            <input type="text" class="form-control" name="no_ktp" id="no_ktp" placeholder="no_ktp" value="<?php echo $no_ktp ?>">
+            <input type="number" class="form-control" required name="no_ktp" id="no_ktp" placeholder="no_ktp" value="<?php echo $no_ktp ?>">
         </div>
 
         <div class="row mt-3">
             <label for="no_hp" class="form-label fw-bold">
                 Nomor Hp
             </label>
-            <input type="text" class="form-control" name="no_hp" id="no_hp" placeholder="no_hp" value="<?php echo $no_hp ?>">
+            <input type="number" class="form-control" required name="no_hp" id="no_hp" placeholder="no_hp" value="<?php echo $no_hp ?>">
         </div>
 
         <div class="row mt-3">
             <label for="no_rm" class="form-label fw-bold">
                 Nomor RM
             </label>
-            <input type="text" class="form-control" name="no_rm" id="no_rm" placeholder="no_rm" value="<?php echo $no_rm ?>">
+            <input type="text" class="form-control" required name="no_rm" id="no_rm" placeholder="no_rm" value="<?php echo $no_rm ?>">
         </div>
 
 
         <div class="row d-flex mt-3 mb-3">
-          <button type="submit" class="btn btn-primary" style="width: 3cm;" name="simpan">Simpan</button>
+          <button type="submit" class="btn btn-primary" style="width: 3cm;" required name="simpan">Simpan</button>
         </div>
 </form>
 
 <div class="row d-flex mt-3 mb-3">
-  <a href="<?= $base_admin.'/dokter' ?>">
+  <a href="<?= $base_admin.'/Pasien' ?>">
     <button class="btn btn-secondary ml-2" style="width: 3cm;">Reset</button>
   </a>
 </div>
@@ -168,7 +185,7 @@ ob_start();
             $stmt->bindParam(':alamat', $_POST['alamat'], PDO::PARAM_STR);
             $stmt->bindParam(':no_ktp', $_POST['no_ktp'], PDO::PARAM_INT);
             $stmt->bindParam(':no_hp', $_POST['no_hp'], PDO::PARAM_INT);
-            $stmt->bindParam(':no_rm', $_POST['no_rm'], PDO::PARAM_INT);
+            $stmt->bindParam(':no_rm', $_POST['no_rm'], PDO::PARAM_STR);
             $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);
             $stmt->execute();
 
@@ -182,7 +199,7 @@ ob_start();
             $stmt->bindParam(':alamat', $_POST['alamat'], PDO::PARAM_STR);
             $stmt->bindParam(':no_ktp', $_POST['no_ktp'], PDO::PARAM_INT);
             $stmt->bindParam(':no_hp', $_POST['no_hp'], PDO::PARAM_INT);
-            $stmt->bindParam(':no_rm', $_POST['no_rm'], PDO::PARAM_INT);
+            $stmt->bindParam(':no_rm', $_POST['no_rm'], PDO::PARAM_STR);
             $stmt->execute();
 
             header('Location:index.php');
