38baf521c4b9c546888c1cec9d128cff56718e64
samuelandrey <samuelandrey2021@gmail.com>
2023-12-29 12:27:39 +0700

    return the field of select schedule

--- pages/pasien/poli/get_jadwal.php
+++ pages/pasien/poli/get_jadwal.php
@@ -7,7 +7,9 @@ $poliId = isset($_GET['poli_id']) ? $_GET['poli_id'] : null;
 // Query untuk mendapatkan jadwal dokter berdasarkan ID poli
 $dataJadwal = $pdo->prepare("SELECT a.nama as nama_dokter, 
                                     b.hari as hari, 
-                                    b.id as id_jp
+                                    b.id as id_jp,
+                                    b.jam_mulai as jam_mulai,
+                                    b.jam_selesai as jam_selesai
 
                                     FROM dokter as a
 
@@ -19,7 +21,7 @@ $dataJadwal->execute();
 
 // Bangun opsi-opsi jadwal dokter
 if ($dataJadwal->rowCount() == 0) {
-  echo '<option>Tidak ada jadwal</option>';
+    echo '<option>Tidak ada jadwal</option>';
 } else {
   while ($jd = $dataJadwal->fetch()) {
     echo '<option value="' . $jd['id_jp'] . '"> Dokter ' . $jd['nama_dokter'] . ' | ' . $jd['hari'] . ' | ' . $jd['jam_mulai'] . ' - ' . $jd['jam_selesai'] .'</option>';
